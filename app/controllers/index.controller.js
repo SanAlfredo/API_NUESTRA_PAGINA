ca795875563d613b267a068a4820825b36865dc7
SanAlfredo <alfredo.2009.8@gmail.com>
2024-02-20 18:09:58 -0400

    V 0, configuracion inicial y conexion mysql

---
+++ app/controllers/index.controller.js
@@ -0,0 +1,23 @@
+import { conn } from "../models/conexion.model.js";
+//import "../models/index.model.js";
+
+export const ping = (req, res) => {
+  if (conn) {
+    res.send({
+      message:"Conectado con exito"
+    });
+  } else {
+    res.status(500).send({message: "No se ha podido conectar"});
+  }
+};
+export const index = (req, res) => {
+  res.json({
+    message: "Bienvenido a la pagina",
+    data: {
+      nameSystem: "backend api nuestra pagina",
+      version: "0.0.1",
+      developer: "Alfredo Valverde Aranibar",
+      email: "alfredo.2009.8@gmail.com",
+    },
+  });
+};
